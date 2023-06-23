2300043b620f9549b3c5981e6d2da4a587f6664e
mikhailVershkov91 <mikhail.vershkov1991@gmail.com>
2023-06-23 23:55:23 +0600

    favorites functionality

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -2,6 +2,7 @@
 
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { SafeUser } from "../types";
+import { useFavorites } from "../hooks/useFavorites";
 
 interface Props {
 	listingId: string;
@@ -9,8 +10,10 @@ interface Props {
 }
 
 export const HeartButton: React.FC<Props> = ({ listingId, currentUser }) => {
-	const hasFavorited = false;
-	const toggleFavorite = () => {};
+	const { hasFavorited, toggleFavorite } = useFavorites({
+		listingId,
+		currentUser,
+	});
 
 	return (
 		<div
